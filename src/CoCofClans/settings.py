5726b143d335e34ed39e0c2e59915c562ec466bd
Diego Puente <21991012+DiegoPGs@users.noreply.github.com>
2023-08-13 19:24:07 -0600

    Placeholders

---
+++ src/CoCofClans/settings.py
@@ -0,0 +1,13 @@
+"""
+settings.py contains all of the clash of clans variables we want to direct the use of our library, such as
+1. Clan
+2. Player list
+"""
+
+# The clan we want to scrap information from
+clan = ''
+
+# The players that we are interested in
+players = (
+    
+)
---
+++ src/CoCofSecrets/APIsecrets.py
@@ -0,0 +1,9 @@
+"""
+Store your secrets here
+"""
+
+# clash of clans API KEY
+COC_KEY = ''
+
+# google sheets API KEY
+GOOGLE_KEY = ''
